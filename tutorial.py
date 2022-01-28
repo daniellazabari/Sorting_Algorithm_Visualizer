ee3afb9f86e489df9d009cf7c5f7a9776a6ed73b
daniella.zabari <daniella.zabari@post.idc.ac.il>
2022-01-28 22:55:41 +0200

    created __init__

--- tutorial.py
+++ tutorial.py
@@ -5,7 +5,18 @@ pygame.init()
 class DrawInformation:
     BLACK = 0,0,0
     WHITE = 255, 255, 255
-    RED = 255, 0, 0
-    GREEN = 0, 255, 0
-    BLUE = 0, 0, 255
-    
+    PINK = 239, 170, 196
+    GREY = 107, 113, 126
+    LIGHT_PINK = 255, 196, 209
+    ORANGE = 255, 232, 225
+    GREEN = 212, 220, 205
+    BACKGROUND_COLOR = WHITE
+
+    #lst is the list that we will sort
+    def __init__(self, width, height, lst):
+        self.width = width
+        self.height = height
+
+        
+
+
