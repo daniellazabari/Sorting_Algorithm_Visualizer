5d2de2026615ff8df8ca7788f7e18ba9073d3cea
daniella.zabari <daniella.zabari@post.idc.ac.il>
2022-01-30 18:38:29 +0200

    bubble sort complete

--- tutorial.py
+++ tutorial.py
@@ -1,3 +1,4 @@
+import math
 from turtle import width
 import pygame
 import random
@@ -46,7 +47,7 @@ class DrawInformation:
         # Calculate the width of each block based on the size of 
         # the screen and the number of blocks we will have
         self.block_width = round((self.width - self.SIDE_PAD) / len(lst))
-        self.block_height = round((self.height - self.TOP_PAD) / (self.max_val - self.min_val))
+        self.block_height = math.floor((self.height - self.TOP_PAD) / (self.max_val - self.min_val))
         # Set drawing starting point 
         self.start_x = self.SIDE_PAD // 2
 
@@ -58,24 +59,33 @@ def draw(draw_info):
     controls = draw_info.FONT.render("R - Reset | SPACE - Start Sorting | A - Ascending | D - Descending", 1, draw_info.BLACK)
     draw_info.window.blit(controls, ((draw_info.width - controls.get_width())/2, 10))
     sorting = draw_info.FONT.render("I - Insertion Sort | B - Buble Sort", 1, draw_info.BLACK)
-    draw_info.window.blit(sorting, ((draw_info.width - sorting.get_width())/2, 45))
+    draw_info.window.blit(sorting, ((draw_info.width - sorting.get_width())/2, 40))
 
 
     draw_list(draw_info)
     pygame.display.update()
 
 # Draw the list that we are sorting
-def draw_list(draw_info):
+def draw_list(draw_info, color_positions={}, clear_bg= False):
     lst = draw_info.lst
+
+    if clear_bg:
+        clear_rect = (draw_info.SIDE_PAD // 2, draw_info.TOP_PAD, draw_info.width - draw_info.SIDE_PAD, draw_info.height - draw_info.TOP_PAD)
+        pygame.draw.rect(draw_info.window, draw_info.BACKGROUND_COLOR, clear_rect)
+
 
     for i, val in enumerate(lst):
         x = draw_info.start_x + i * draw_info.block_width
         y = draw_info.height - (val - draw_info.min_val) * draw_info.block_height
         color = draw_info.GRADIENTS[i % 3]
 
-        # Draw rectangle blocks 
+        # Draw rectangle blocks
+        if i in color_positions:
+            color = color_positions[i]
         pygame.draw.rect(draw_info.window, color, (x, y, draw_info.block_width, draw_info.height))
 
+    if clear_bg:
+        pygame.display.update()
 
 
 # Generate initial list that contains n random integer elements 
@@ -88,6 +98,20 @@ def generate_starting_list(n, min_val, max_val):
         lst.append(val)
 
     return lst
+
+def bubble_sort(draw_info, ascending = True):
+    lst = draw_info.lst
+    # [2, 3, 5, 8, 3, 1, 4]
+    for i in range(len(lst) - 1):
+        for j in range(len(lst) - 1 - i):
+            num1 = lst[j]
+            num2 = lst[j + 1]
+            if (num1 > num2 and ascending) or (num1 < num2 and not ascending):
+                lst[j], lst[j+1] = lst[j + 1], lst[j]
+                draw_list(draw_info, {j: draw_info.GREEN, j + 1: draw_info.PINK}, True)
+                yield True
+    return lst
+            
 
 def main():
     run = True
@@ -104,10 +128,20 @@ def main():
     sorting = False
     ascending = True
 
+    sorting_algorithm = bubble_sort
+    sorting_algo_name = "Bubble Sort"
+    sorting_algorithm_generator = None
+
     while run:
         clock.tick(60)
-
-        draw(draw_info)
+        
+        if sorting:
+            try:
+                next(sorting_algorithm_generator)
+            except StopIteration:
+                sorting = False
+        else:
+            draw(draw_info)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -122,10 +156,10 @@ def main():
                 lst = generate_starting_list(n, min_val, max_val)
                 draw_info.set_list(lst)
                 sorting = False
-            
             # Else if space key is pressed- start sorting  
             elif event.key == pygame.K_SPACE and sorting == False:
                 sorting = True
+                sorting_algorithm_generator = sorting_algorithm(draw_info, ascending)
             
             # Else if "a" key is pressed- sort in ascending order  
             elif event.key == pygame.K_a and not sorting:
